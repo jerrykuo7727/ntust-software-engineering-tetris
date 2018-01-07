4216352391b32436c44df0f58c08d486075b29ea
B10432003 <35024150+B10432003@users.noreply.github.com>
2018-01-07 16:04:34 +0800

    Model B10432003部分
    大概這樣 有問題請說

--- Model.cs
+++ Model.cs
@@ -2,40 +2,67 @@
 class Model
 {
 	/* On board, 0: space, 1: falling block, 2: rotation core, 3: piling block, 4: border */
+	//initial the board = 0, and the boarder = 4
 	public void BOARD_init()
 	{
 		int i, j;
 		for (i = 0; i < 21; i++)
 			for (j = 0; j < 12; j++)
 			{
-				if (i == 20) BOARD[i][j] = 4;
-				else if (j == 0 || j == 11) BOARD[i][j] = 4;
-				else BOARD[i][j] = 0;
+				if (i == 20) BOARD[i,j] = 4;
+				else if (j == 0 || j == 11) BOARD[i,j] = 4;
+				else BOARD[i,j] = 0;
 			}
+
+		display();
 	}
-	public void add_block(int piece, int rotation)//block init
+	//add a new block in the board
+	public void add_block(int piece, int rotation)//block_init
 	{
 		int i, j;
 		for (i = 0; i < 4; i++)
 			for (j = 0; j < 4; j++)
-				if (BOARD[i][j + 3] == 0)
-					BOARD[i][j + 3] = PIECES[piece][rotation][i][j];
+				if (BOARD[i,j + 3] == 0)
+					BOARD[i,j + 3] = PIECES[piece,rotation,i,j];
+
+		display();
 	}
-	public int can_left()
+
+	void block_fall()
 	{
 		int i, j;
 		for (i = 19; i >= 0; i--)
 		{
 			for (j = 1; j < 11; j++)
 			{
-				if (BOARD[i][j] >= 1 && BOARD[i][j] <= 2 && BOARD[i][j - 1] > 2) return 0;
+				if (BOARD[i,j] == 1 || BOARD[i,j] == 2)
+				{
+					BOARD[i + 1,j] = BOARD[i,j];  // replace the next line with current line
+					BOARD[i,j] = 0; // clean the current line
+				}
 			}
 		}
-		return 1;
+		display();
 	}
-	public int can_right( );
-	public int can_rotate( );
-	public int can_fall( );
+
+	// turn falling block into piling block
+	void block_turn()
+	{
+		int i, j;
+		for (i = 19; i >= 0; i--)
+		{
+			for (j = 1; j < 11; j++)
+			{
+				if (BOARD[i,j] == 1 || BOARD[i,j] == 2) BOARD[i,j] = 3;
+			}
+		}
+		display();
+	}
+
+	void block_left( );
+	void block_right( );
+	void block_rotate(int cp, int cr);
+	void block_land();
 	
 
 
