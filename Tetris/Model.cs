9a88dc198ecfb97f5064bcc72be744ea6be58f92
leonw774 <33317554+leonw774@users.noreply.github.com>
2018-01-09 18:02:49 +0800

    Organize codes and Add Comments

--- Tetris/Model.cs
+++ Tetris/Model.cs
@@ -1,39 +1,322 @@
-
 class Model
 {
-	/* On board, 0: space, 1: falling block, 2: rotation core, 3: piling block, 4: border */
-	//initial the board = 0, and the boarder = 4
-	public void BOARD_init()
-	{
-		int i, j;
-		for (i = 0; i < 21; i++)
-			for (j = 0; j < 12; j++)
+    //Types of the tetris model
+    //0 is sapce,1 is block, 2 is block and the rotate core
+    public readonly int[,,,] PIECES = new int[7, 4, 4, 4]
+    {
+        { // O
+	        {
+                { 0, 0, 0, 0 },
+                { 0, 1, 2, 0 },
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 0, 0 },
+                { 0, 1, 2, 0 },
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 0, 0 },
+                { 0, 1, 2, 0 },
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 0, 0 },
+                { 0, 1, 2, 0 },
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 }
+            }
+        }, // end O
+	    { // I
 			{
-				if (i == 20) BOARD[i,j] = 4;
-				else if (j == 0 || j == 11) BOARD[i,j] = 4;
-				else BOARD[i,j] = 0;
-			}
+                { 0, 0, 0, 0 },
+                { 1, 1, 2, 1 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 1, 0 },
+                { 0, 0, 2, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 1, 0 }
+            },
+            {
+                { 0, 0, 0, 0 },
+                { 1, 1, 2, 1 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 1, 0 },
+                { 0, 0, 2, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 1, 0 }
+            }
+        }, // end I
+		{ // S
+			{
+                { 0, 0, 0, 0 },
+                { 0, 0, 2, 1 },
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 1, 0 },
+                { 0, 0, 2, 1 },
+                { 0, 0, 0, 1 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 0, 0 },
+                { 0, 0, 2, 1 },
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 1, 0 },
+                { 0, 0, 2, 1 },
+                { 0, 0, 0, 1 },
+                { 0, 0, 0, 0 }
+            }
+        }, // end S
+		{ // Z
+			{
+                { 0, 0, 0, 0 },
+                { 0, 1, 2, 0 },
+                { 0, 0, 1, 1 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 0, 1 },
+                { 0, 0, 2, 1 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 0, 0 },
+                { 0, 1, 2, 0 },
+                { 0, 0, 1, 1 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 0, 1 },
+                { 0, 0, 2, 1 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 0 }
+            }
+        }, // end Z
+		{ // L
+			{
+                { 0, 0, 0, 0 },
+                { 0, 1, 2, 1 },
+                { 0, 1, 0, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 1, 0 },
+                { 0, 0, 2, 0 },
+                { 0, 0, 1, 1 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 0, 1 },
+                { 0, 1, 2, 1 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 1, 1, 0 },
+                { 0, 0, 2, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 0 }
+            }
+        }, // end L
+		{ // J
+			{
+                { 0, 0, 0, 0 },
+                { 0, 1, 2, 1 },
+                { 0, 0, 0, 1 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 1, 1 },
+                { 0, 0, 2, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 1, 0, 0 },
+                { 0, 1, 2, 1 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 1, 0 },
+                { 0, 0, 2, 0 },
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 }
+            }
+        }, // end J
+		{ // T
+			{
+                { 0, 0, 0, 0 },
+                { 0, 1, 2, 1 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 1, 0 },
+                { 0, 0, 2, 1 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 1, 0 },
+                { 0, 1, 2, 1 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 }
+            },
+            {
+                { 0, 0, 1, 0 },
+                { 0, 1, 2, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 0 }
+            }
+        } // end T
+	};
+    public int total_del = 0, difficulty = 1; // total of deleted lines
+    public int[,] BOARD; // 20x10 without borders
 
-		display();
-	}
-	//add a new block in the board
-	public void add_block(int piece, int rotation)//block_init
-	{
-		int i, j;
-		for (i = 0; i < 4; i++)
-			for (j = 0; j < 4; j++)
-				if (BOARD[i,j + 3] == 0)
-					BOARD[i,j + 3] = PIECES[piece,rotation,i,j];
+    public Model()
+    {
+        BOARD = new int[21, 12];
+        BOARD.Initialize(); // the default value of int in C# is 0
+    }
 
-		display();
-	}
+    /* On board, 0: space, 1: falling block, 2: rotation core, 3: piling block, 4: border */
+    //initial the board = 0, and the boarder = 4
+    public void BOARD_init()
+    {
+        for (int i = 0; i < 21; i++)
+            for (int j = 0; j < 12; j++)
+            {
+                if (i == 20) BOARD[i, j] = 4;
+                else if (j == 0 || j == 11) BOARD[i, j] = 4;
+                else BOARD[i, j] = 0;
+            }
+    }
+
+    //add a new block in the board
+    public void add_block(int piece, int rotation)//block_init
+    {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                if (BOARD[i, j + 3] == 0)
+                    BOARD[i, j + 3] = PIECES[piece, rotation, i, j];
+    }
+
+
+    void block_left()
+    {
+        for (int i = 19; i >= 0; i--)
+        {
+            for (int j = 1; j < 11; j++)
+            {
+                if (BOARD[i, j] == 1 || BOARD[i, j] == 2)
+                {
+                    BOARD[i, j - 1] = BOARD[i, j];
+                    BOARD[i, j] = 0;
+                }
+            }
+        }
+    }
+
+    void block_right()
+    {
+        for (int i = 19; i >= 0; i--)
+        {
+            for (int j = 10; j > 0; j--)
+            {
+                if (BOARD[i, j] == 1 || BOARD[i, j] == 2)
+                {
+                    BOARD[i, j + 1] = BOARD[i, j];
+                    BOARD[i, j] = 0;
+                }
+            }
+        }
+    }
+
+    void block_rotate(int cp, int cr)
+    {
+        int core_i = 0, core_j = 0;
+        for (int i = 19; i >= 0; i--)
+        { // search the piece core
+            for (int j = 1; j < 11; j++)
+            {
+                if (BOARD[i, j] == 2)
+                {
+                    core_i = i;
+                    core_j = j;
+                    break;
+                } // end if
+            } // end for
+        } // end for
+        if (cp == 1)
+        { // if current piece is I , overwrite 4x4
+            for (int i = core_i - 1; i <= core_i + 2; i++)
+            {
+                for (int j = core_j - 2; j <= core_j + 1; j++)
+                {
+                    BOARD[i, j] = PIECES[cp, cr, i - core_i + 1, j - core_j + 2];
+                } // end for
+            } // end for
+        }
+        else if (cp == 6)
+        { // if current piece is T, overwrite cross only
+            for (int i = core_i - 1, ti = 0; i <= core_i + 1; i++, ti++)
+            {
+                for (int j = core_j - 1, tj = 1; j <= core_j + 1; j++, tj++)
+                {
+                    if (PIECES[cp, cr, ti, tj] > 0 || BOARD[i, j] == 1)
+                        BOARD[i, j] = PIECES[cp, cr, i - core_i + 1, j - core_j + 2];
+                } // end for
+            } // end for
+        }
+        else
+        { // other piece except O, overwrite 3x3
+            for (int i = core_i - 1; i <= core_i + 1; i++)
+            {
+                for (int j = core_j - 1; j <= core_j + 1; j++)
+                {
+                    BOARD[i, j] = PIECES[cp, cr, i - core_i + 1, j - core_j + 2];
+                } // end for
+            } // end for
+        } // end else
+    }
+
+    void block_land()
+    {
+        for (int i = 19; i >= 0; i--)
+        {
+            for (int j = 1; j < 11; j++)
+            {
+                if (BOARD[i, j] == 1 || BOARD[i, j] == 2)
+                {
+                    BOARD[i + 1, j] = BOARD[i, j];  // replace the next line with current line
+                    BOARD[i, j] = 0; // clean the current line
+                }
+            }
+        }
+    }
 
 	public void block_fall()
 	{
-		int i, j;
-		for (i = 19; i >= 0; i--)
+		for (int i = 19; i >= 0; i--)
 		{
-			for (j = 1; j < 11; j++)
+			for (int j = 1; j < 11; j++)
 			{
 				if (BOARD[i,j] == 1 || BOARD[i,j] == 2)
 				{
@@ -42,219 +325,18 @@ class Model
 				}
 			}
 		}
-		display();
 	}
 
 	// turn falling block into piling block
 	public void block_turn()
 	{
-		int i, j;
-		for (i = 19; i >= 0; i--)
+		for (int i = 19; i >= 0; i--)
 		{
-			for (j = 1; j < 11; j++)
+			for (int j = 1; j < 11; j++)
 			{
 				//turn it to piling block
 				if (BOARD[i,j] == 1 || BOARD[i,j] == 2) BOARD[i,j] = 3;
 			}
 		}
-		display();
 	}
-	public void block_left( );
-	public void block_right( );
-	public void block_rotate(int cp, int cr);
-	public void block_land();
-	
-
-	public int total_del = 0, difficulty = 1, speed = 1000; // total of deleted lines
-	public int[,] BOARD = new int[21, 12] { 0 }; // 20x10 without borders
-
-	//Types of the tetris model
-	//0 is sapce,1 is block, 2 is block and the rotate core
-	public int[,,,] PIECES = new int[7, 4, 4, 4]
-	{
-			{ // O
-	        	{
-					{ 0, 0, 0, 0 },
-					{ 0, 1, 2, 0 },
-					{ 0, 1, 1, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 0, 0 },
-					{ 0, 1, 2, 0 },
-					{ 0, 1, 1, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 0, 0 },
-					{ 0, 1, 2, 0 },
-					{ 0, 1, 1, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 0, 0 },
-					{ 0, 1, 2, 0 },
-					{ 0, 1, 1, 0 },
-					{ 0, 0, 0, 0 }
-				}
-			}, // end O
-	        { // I
-				{
-					{ 0, 0, 0, 0 },
-					{ 1, 1, 2, 1 },
-					{ 0, 0, 0, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 2, 0 },
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 1, 0 }
-				},
-				{
-					{ 0, 0, 0, 0 },
-					{ 1, 1, 2, 1 },
-					{ 0, 0, 0, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 2, 0 },
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 1, 0 }
-				}
-			}, // end I
-			{ // S
-				{
-					{ 0, 0, 0, 0 },
-					{ 0, 0, 2, 1 },
-					{ 0, 1, 1, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 2, 1 },
-					{ 0, 0, 0, 1 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 0, 0 },
-					{ 0, 0, 2, 1 },
-					{ 0, 1, 1, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 2, 1 },
-					{ 0, 0, 0, 1 },
-					{ 0, 0, 0, 0 }
-				}
-			}, // end S
-			{ // Z
-				{
-					{ 0, 0, 0, 0 },
-					{ 0, 1, 2, 0 },
-					{ 0, 0, 1, 1 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 0, 1 },
-					{ 0, 0, 2, 1 },
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 0, 0 },
-					{ 0, 1, 2, 0 },
-					{ 0, 0, 1, 1 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 0, 1 },
-					{ 0, 0, 2, 1 },
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 0, 0 }
-				}
-			}, // end Z
-			{ // L
-				{
-					{ 0, 0, 0, 0 },
-					{ 0, 1, 2, 1 },
-					{ 0, 1, 0, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 2, 0 },
-					{ 0, 0, 1, 1 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 0, 1 },
-					{ 0, 1, 2, 1 },
-					{ 0, 0, 0, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 1, 1, 0 },
-					{ 0, 0, 2, 0 },
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 0, 0 }
-				}
-			}, // end L
-			{ // J
-				{
-					{ 0, 0, 0, 0 },
-					{ 0, 1, 2, 1 },
-					{ 0, 0, 0, 1 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 1, 1 },
-					{ 0, 0, 2, 0 },
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 1, 0, 0 },
-					{ 0, 1, 2, 1 },
-					{ 0, 0, 0, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 2, 0 },
-					{ 0, 1, 1, 0 },
-					{ 0, 0, 0, 0 }
-				}
-			}, // end J
-			{ // T
-				{
-					{ 0, 0, 0, 0 },
-					{ 0, 1, 2, 1 },
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 2, 1 },
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 1, 0 },
-					{ 0, 1, 2, 1 },
-					{ 0, 0, 0, 0 },
-					{ 0, 0, 0, 0 }
-				},
-				{
-					{ 0, 0, 1, 0 },
-					{ 0, 1, 2, 0 },
-					{ 0, 0, 1, 0 },
-					{ 0, 0, 0, 0 }
-				}
-			} // end T
-        
-	};
-
 }
--- Tetris/View.cs
+++ Tetris/View.cs
@@ -18,7 +18,7 @@ namespace Tetris
         Timer timer;
         public USERINPUT input;
 
-        protected Color controllerColor;
+        protected Color controllerColor; // just for test
         protected int TileHeight = 16;
         protected int TileWidth = 16;
 
@@ -27,16 +27,17 @@ namespace Tetris
             //model = new Model();
             //controller = new Controller(model);
             controllerColor = Color.DarkCyan;
-            timer = new Timer() { Interval = 1000 };
+
+            timer = new Timer() { Interval = 1000 }; // 一秒執行一次Tick事件
             timer.Tick += delegate{
-                input = USERINPUT.DOWN;
+                input = USERINPUT.DOWN; // 在Tick發生之後要做{input = USERINPUT.DOWN;}
             };
-            timer.Tick += (s, e) => Invalidate();
+            timer.Tick += (s, e) => Invalidate();// 然後再來要做{Invalidate();} Invalidate裡面會呼叫OnPaint
             timer.Start();
         }
 
-        // pretend to be controller for a while
-        public void ControllerUpdate()
+        // just pretend to be controller for a while
+        public void UserHasInput()
         {
             if (controllerColor == Color.DarkCyan)
                 controllerColor = Color.DarkBlue;
@@ -46,17 +47,17 @@ namespace Tetris
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            // Controller.UserHasInput();
-            ControllerUpdate();
-            GameDisplay(e);
-            base.OnPaint(e);
+            // Controller.UserHasInput(); // 呼叫Controller遊戲要更新
+            UserHasInput();
+            GameDisplay(e); // 進入這個函式以畫出畫面，這是大家在自己的View裡要override的
+            base.OnPaint(e); // 回去做它沒有被override時會做的事
         }
 
         protected virtual void GameDisplay(PaintEventArgs e)
         {
-            for (int i = 1; i < 21 /* Model.BOARD_HEIGHT */; i++)
+            for (int i = 19; i > 0; i++)
             {
-                for (int j = 1; j < 11 /* Model.BOARD_WIDTH */; j++)
+                for (int j = 10; j > 0; j++)
                 {
                     e.Graphics.DrawRectangle(new Pen(new SolidBrush(Color.Black)),
                                              new Rectangle(j * TileWidth, i * TileHeight, TileWidth - 1, TileHeight - 1));
